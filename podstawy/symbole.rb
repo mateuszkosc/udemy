3deea19618f29866d9859079c3c5cb4c1507ad99
mateuszkosc <m.kosc@nomtek.com>
2018-11-09 08:54:16 +0100

    symbole opisy

--- podstawy/symbole.rb
+++ podstawy/symbole.rb
@@ -1,6 +1,9 @@
 :test
 p :test.class
 
+#  symbole mają zawsze to samo id obiektu czyli są przechowywane w tym samym miejscu w pamięci
+# każdy string ma inne id obiektu czyli jest przechowywany w innym miejscu w pamięci
+
 p :test.object_id
 p :test.object_id
 
